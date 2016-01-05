3ef2009e5df5307156cc9775301e69f411c278a3
马乐 <mihaly960308@live.com>
2016-01-05 13:29:24 +0800

    1.0.0

---
+++ MicroZone/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MicroZone.Startup))]
+namespace MicroZone
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
